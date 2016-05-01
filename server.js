7f5d7951fe535bc4c8753731781e5931d9d56db2
YuLun Shih <shih@yulun.me>
2016-05-02 02:09:13 +0800

    Update

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var app = require('./app');
 var port = process.env.PORT || 8000;
-
-app.listen(port, function () {
+var ip = process.env.IP || '0.0.0.0';
+app.listen(port, ip, function () {
   console.log('Listen on: %s', port);
 });
